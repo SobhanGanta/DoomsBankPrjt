8925180f3dad746571cdecb7e49e2e1eb3f62836
SobhanGanta <sobhan.ganta@gmail.com>
2016-12-19 20:04:53 +0530

    added modifier public

---
+++ cycloneBank/src/main/java/com/cts/doomsbank/cycloneBank/CreditCard.java
@@ -0,0 +1,11 @@
+package com.cts.doomsbank.cycloneBank;
+
+public class CreditCard {
+
+	void getDetails(){
+		System.out.println("Hello user");
+	}
+	public void getCardId(){
+		System.out.println("656788");
+	}
+}
